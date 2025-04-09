c9fabe58f2a6fe78c6835ca32902e07c4c21a30b
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2025-04-09 19:05:24 +0200

    create chapter card component

---
+++ src/components/ChapterCard/ChapterCard.tsx
@@ -0,0 +1,50 @@
+import Link from "@docusaurus/Link";
+import styles from "./ChapterCard.module.css";
+
+// Rendering Cards to structure Headings and Pages
+export function ChapterCard({
+  chapter,
+  sectionName,
+  chapterIndex,
+}: {
+  chapter: any;
+  sectionName: string;
+  chapterIndex: number;
+}) {
+  return (
+    <div className={styles.card}>
+      {chapter.title && (
+        <h3 className={styles.cardTitle}>
+          {sectionName.toLowerCase() === "guides" && `${chapterIndex}. `}
+          <Link to={`/docs/${toKebabCase(chapter.title)}`}>
+            {chapter.title}
+          </Link>
+        </h3>
+      )}
+      <ul className={styles.pageList}>
+        {chapter.pages.map((page: string, pageIndex: number) => (
+          // For guides, display numbers in front of the chapter and page titles.
+          <li key={page}>
+            {sectionName.toLowerCase() === "guides" && (
+              <span className={styles.numbering}>
+                {`${chapterIndex}.${pageIndex + 1} `}
+              </span>
+            )}
+            <Link
+              to={`/docs/${toKebabCase(
+                chapter.title || sectionName
+              )}/${toKebabCase(page)}`}
+            >
+              {page}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+// Helper function for link path creation
+function toKebabCase(str: string): string {
+  return str.toLowerCase().replace(/\s+/g, "-");
+}
