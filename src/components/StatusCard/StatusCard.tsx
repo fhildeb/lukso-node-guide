2a6b77cacbcdf767085730f798e96f65286e7262
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2025-07-30 18:13:04 +0200

    update status panel

--- src/components/StatusCard/StatusCard.tsx
+++ src/components/StatusCard/StatusCard.tsx
@@ -6,7 +6,7 @@ export function StatusCard() {
   return (
     <div className={styles.card}>
       <div>
-        This guide was last updated and tested on July 8, 2025 using the
+        This guide was last updated and tested on July 30, 2025 using the
         following software:
       </div>
       {Object.entries(software).map(([category, tools]) => (
